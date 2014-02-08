e7d590297a7205789a4e84e8e7ddca421d0370dd
jasperreyes <reyes.jasper@gmail.com>
2014-02-08 13:58:28 -0600

    project pages

--- portfolio_4.6/script.js
+++ portfolio_4.6/script.js
@@ -1,91 +1,78 @@
 $(document).ready(function(){
 
     //INITIAL SETUP
-    $('#landing_page').show();
+    $('#home_page').show();
 	$('#work_page').hide();
 	$('#about_page').hide();
+    $('#jasper_background').hide();
+    $('#chicago_background').hide();
+    $('#project_hydratile').hide();
 
-    //LANDING PAGE INTERACTIONS
-    // $('#jasper_button').click(function() {
-    //     $('#landing_page').css('background-image', 'url('starvedrock.jpg')');
-    //     $('#landing_page').css('background-size', 'cover');
-    //     $('#landing_page').css('background-position', 'center center');
-    // });
+    //NAVBAR
+    $('#home_navbar').css('top','-60px');
+    $('#home_navbar').delay(800).animate({'top':'0px'},400);
 
-    //INTRO TAB ANIMATIONS
-    $('#landing_navbar').css('top','-60px');
-    $('#landing_navbar').delay(800).animate({'top':'0px'},400);
+    //HOME INTERACTIONS
+    $('#jasper_button').click(function() {
+        $('#jasper_background').fadeIn(500).delay(800).fadeOut(500);
+    });
+    $('#chicago_button').click(function() {
+        $('#chicago_background').fadeIn(500).delay(800).fadeOut(500);
+    });
 
-    // //HOVER TAB ANIMATIONS
-    // $('.tab_leftwhite').mouseenter(function() {
-    //  $('.tab_leftwhite').delay(100).animate({'left':'0'},100);
-    // });
-    // $('.tab_leftwhite').mouseleave(function() {
-    //  $('.tab_leftwhite').animate({'left':'-20px'},100);
-    // });
-    // $('.tab_rightwhite').mouseenter(function() {
-    //  $('.tab_rightwhite').delay(100).animate({'right':'0'},100);
-    // });
-    // $('.tab_rightwhite').mouseleave(function() {
-    //  $('.tab_rightwhite').animate({'right':'-20px'},100);
-    // });
-    // $('.tab_leftgrey').mouseenter(function() {
-    //  $('.tab_leftgrey').delay(100).animate({'left':'0px'},100);
-    // });
-    // $('.tab_leftgrey').mouseleave(function() {
-    //  $('.tab_leftgrey').animate({'left':'-20px'},100);
-    // });
-    // $('.tab_rightgrey').mouseenter(function() {
-    //  $('.tab_rightgrey').delay(100).animate({'right':'0px'},100);
-    // });
-    // $('.tab_rightgrey').mouseleave(function() {
-    //  $('.tab_rightgrey').animate({'right':'-20px'},100);
-    // });
+    //PROJECT TRANSITIONS
+    $('#work_thumbnail_hydratile').click(function() {
+        $('#work_container').fadeOut();
+        $('#project_hydratile').fadeIn();
+    });
+    $('#project_hydratile_exitbutton').click(function(){
+        $('#project_hydratile').fadeOut();
+        $('#work_container').fadeIn();
+    });
 
-    //HOME->ABOUT TRANSITION
-    // $('#home_aboutbutton').click(function() {
-    //  $('#home_page').animate({'left':'100%'},400);
-    //  $('#home_page').delay(400).hide(0);
-    //  $('#about_page').show();
-     // $('#home_aboutbutton').delay(400).hide(0);
-     // $('#home_workbutton').delay(400).hide(0);
-
-    //  $('#about').show();
-    //  $('#about').css('left','-100%');
-    //  $('#about').animate({'left':'0%'},400);
-    //  $('#about_homebutton').css('right','-100px');
-    //  $('#about_homebutton').delay(400).animate({'right':'-20px'});
-    // });
-
-    // //ABOUT->HOME TRANSITION
-    // $('#about_homebutton').click(function() {
-
-    //  $('#about').animate({'left':'-100%'},400);
-    //  $('#about').delay(400).hide(0);
-
-    //  $('#home').show();
-    //  $('#home').css('left','100%');
-    //  $('#home').animate({'left':'0%'},400);
-
-    //  $('#home_aboutbutton').delay(400).show(0);
-    //  $('#home_aboutbutton').css('left','-100px');     
-    //  $('#home_aboutbutton').delay(400).animate({'left':'-20px'},400);
-
-    //  $('#home_workbutton').delay(400).show(0);
-    //  $('#home_workbutton').css('right','-100px');
-    //  $('#home_workbutton').delay(400).animate({'right':'-20px'},400);
-    // });
-
-    //LANDING->WORK TRANSITION
-    $('#landing_workbutton').click(function() {
-     $('#landing_page').animate({'left':'-100%'},400);
-     $('#landing_page').delay(400).hide(0);
+    //HOME-->WORK TRANSITION
+    $('#home_workbutton').click(function() {
+     $('#home_page').animate({'left':'-100%'},400);
+     $('#home_page').delay(400).hide(0);
      $('#work_page').show();
      $('#work_page').css('left','100%');
      $('#work_page').animate({'left':'0%'},400);
-     $('#landing_navbar').hide();
+     $('#home_navbar').hide();
      $('#work_navbar').show();
     });
+
+    //WORK-->HOME TRANSITION
+     $('#work_homebutton').click(function() {
+     $('#work_page').animate({'left':'100%'},400);
+     $('#work_page').delay(400).hide(0);
+     $('#home_page').show();
+     $('#home_page').css('left','-100%');
+     $('#home_page').animate({'left':'0%'},400);
+     $('#work_navbar').hide();
+     $('#home_navbar').show();
+    });   
+
+    //HOME-->ABOUT TRANSITION
+    $('#home_aboutbutton').click(function() {
+     $('#home_page').animate({'left':'100%'},400);
+     $('#home_page').delay(400).hide(0);
+     $('#about_page').show();
+     $('#about_page').css('left','-100%');
+     $('#about_page').animate({'left':'0%'},400);
+     $('#home_navbar').hide();
+     $('#about_navbar').show();
+    });
+
+    //ABOUT-->HOME TRANSITION
+     $('#about_homebutton').click(function() {
+     $('#about_page').animate({'left':'-100%'},400);
+     $('#about_page').delay(400).hide(0);
+     $('#home_page').show();
+     $('#home_page').css('left','100%');
+     $('#home_page').animate({'left':'0%'},400);
+     $('#about_navbar').hide();
+     $('#home_navbar').show();
+    });   
 
     //WORK-->ABOUT TRANSITION
     $('#work_aboutbutton').click(function() {
@@ -95,17 +82,6 @@ $(document).ready(function(){
      $('#about_page').css('left','-100%');
      $('#about_page').animate({'left':'0%'},400);
      $('#work_navbar').hide();
-     $('#about_navbar').show();
-    });
-
-    //LANDING-->ABOUT TRANSITION
-    $('#landing_aboutbutton').click(function() {
-     $('#landing_page').animate({'left':'100%'},400);
-     $('#landing_page').delay(400).hide(0);
-     $('#about_page').show();
-     $('#about_page').css('left','-100%');
-     $('#about_page').animate({'left':'0%'},400);
-     $('#landing_navbar').hide();
      $('#about_navbar').show();
     });
 
@@ -120,15 +96,6 @@ $(document).ready(function(){
      $('#work_navbar').show();
     });
 
-
-    // //WORK->HOME TRANSITION
-    // $('#work_homebutton').click(function() {
-    //  $('#work').animate({'left':'100%'},400);
-    //  $('#work').delay(400).hide(0);
-    //  $('#home').show();
-    //  $('#home').css('left','-100%');
-    //  $('#home').animate({'left':'0%'},400);
-    // });
 
     
 });
