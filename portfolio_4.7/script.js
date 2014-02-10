2e0657b762c3223d4a95ad4ca4357a039c5f650b
jasperreyes <reyes.jasper@gmail.com>
2014-02-10 17:36:40 -0600

    added current page navbar highlight

--- portfolio_4.7/script.js
+++ portfolio_4.7/script.js
@@ -4,6 +4,7 @@ $(document).ready(function(){
     $('#home_page').show();
 	$('#work_page').hide();
 	$('#about_page').hide();
+    $('#homebutton_grey').hide();
     $('#jasper_background').hide();
     $('#chicago_background').hide();
     $('#project_hydratile').hide();
@@ -15,13 +16,10 @@ $(document).ready(function(){
     //HOME INTERACTIONS -------------------------------------------------------------
     $('#jasper_button').click(function() {
         $('#jasper_background').fadeIn(500).delay(800).fadeOut(500);
-        $('#home_container').animate({'color':'#FFF'},500).delay(800).animate({'color':'#222'},500);
-        $(this).animate({'color':'#66cc33'},500).delay(800).animate({'color':'#777'},500);
     });
+
     $('#chicago_button').click(function() {
         $('#chicago_background').fadeIn(500).delay(800).fadeOut(500);
-        $('#home_container').animate({'color':'#FFF'},500).delay(800).animate({'color':'#222'},500);
-        $(this).animate({'color':'#66cc33'},500).delay(800).animate({'color':'#777'},500);
     });
 
     //PROJECT TRANSITIONS -------------------------------------------------------------
@@ -44,7 +42,12 @@ $(document).ready(function(){
             $('#work_page').show();
             $('#work_page').css('left','100%');
             $('#work_page').animate({'left':'0%'},400);
+
+            homeactive = false;
+            workactive = true;
+            aboutactive = false;
         };
+
         if (aboutactive)
         {
             $('#about_page').animate({'left':'-100%'},400);
@@ -52,15 +55,26 @@ $(document).ready(function(){
             $('#work_page').show();
             $('#work_page').css('left','100%');
             $('#work_page').animate({'left':'0%'},400);
+
+            homeactive = false;
+            workactive = true;
+            aboutactive = false;
         };
 
-        homeactive = false;
-        workactive = true;
-        aboutactive = false;
+        $(this).css('color','#66cc33');
+        $('#aboutbutton').css('color','#222');
+        $('#homebutton_green').fadeOut();
+        $('#homebutton_grey').fadeIn();
     });
 
     $('#aboutbutton').click(function()
     {
+
+        $(this).css('color','#66cc33');
+        $('#workbutton').css('color','#222');
+        $('#homebutton_green').fadeOut();
+        $('#homebutton_grey').fadeIn();
+
         if (homeactive)
         {
             $('#home_page').animate({'left':'100%'},400);
@@ -68,7 +82,12 @@ $(document).ready(function(){
             $('#about_page').show();
             $('#about_page').css('left','-100%');
             $('#about_page').animate({'left':'0%'},400);
+           
+            homeactive = false;
+            workactive = false;
+            aboutactive = true;
         };
+
         if (workactive)
         {
             $('#work_page').animate({'left':'100%'},400);
@@ -76,15 +95,22 @@ $(document).ready(function(){
             $('#about_page').show();
             $('#about_page').css('left','-100%');
             $('#about_page').animate({'left':'0%'},400);
+
+            homeactive = false;
+            workactive = false;
+            aboutactive = true;
         };
 
-        homeactive = false;
-        workactive = false;
-        aboutactive = true;
     });
 
     $('#homebutton').click(function()
     {
+
+        $('#homebutton_grey').fadeOut();
+        $('#homebutton_green').fadeIn();
+        $('#aboutbutton').css('color','#222');
+        $('#workbutton').css('color','#222');
+
         if (workactive)
         {
             $('#work_page').animate({'left':'100%'},400);
@@ -92,6 +118,10 @@ $(document).ready(function(){
             $('#home_page').show();
             $('#home_page').css('left','-100%');
             $('#home_page').animate({'left':'0%'},400);
+
+            homeactive = true;
+            workactive = false;
+            aboutactive = false;
         };
         if (aboutactive)
         {
@@ -100,31 +130,40 @@ $(document).ready(function(){
             $('#home_page').show();
             $('#home_page').css('left','100%');
             $('#home_page').animate({'left':'0%'},400);
-        };
 
-        homeactive = true;
-        workactive = false;
-        aboutactive = false;
+            homeactive = true;
+            workactive = false;
+            aboutactive = false;
+        };
+        if (homeactive)
+        {
+            $('#about_page').noop();
+            $('#work_page').noop();
+            $('#home_page').noop();
+
+            homeactive = true;
+            workactive = false;
+            aboutactive = false;
+        };
     });
 
-    //EXIT PROJECT
-        //EXIT BUTTON
-        $('.project_exitbutton').click(function(){
-            $('.project').fadeOut();
-            $('#work_container').fadeIn();
-        });
+    //EXIT BUTTON
+    $('.project_exitbutton').click(function(){
+        $('.project').fadeOut();
+        $('#work_container').fadeIn();
+    });
 
-        //ESCAPE KEYPRESS
-            $(document).keyup(function(e) {
-                if (workactive = true)
-                {
-                    if (e.keyCode == 27)
-                    { 
-                        $('.project').fadeOut();
-                        $('#work_container').fadeIn();
-                    };  
-                };
-            });
+    //ESCAPE KEYPRESS
+    $(document).keyup(function(e) {
+        if (workactive = true)
+        {
+            if (e.keyCode == 27)
+            { 
+                $('.project').fadeOut();
+                $('#work_container').fadeIn();
+            };  
+        };
+    });
 
 
 });
