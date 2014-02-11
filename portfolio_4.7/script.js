07c46a58c33c172cc3819ec320e26617f10f43e9
jasperreyes <reyes.jasper@gmail.com>
2014-02-11 11:50:05 -0600

    fixed navbar issue

--- portfolio_4.7/script.js
+++ portfolio_4.7/script.js
@@ -15,12 +15,12 @@ $(document).ready(function(){
 
     //HOME PAGE INTERACTIONS -------------------------------------------------------------
     $('#home_jasperbutton').click(function() {
-        $('.home_text').animate({'color':'#FFF'},500).delay(800).animate({'color':'#222'});
+        $('.home_text').animate({'color':'#FFF'},500).delay(800).animate({'color':'#777'});
         $('#home_jasperbackground').fadeIn(500).delay(800).fadeOut(500);
     });
 
     $('#home_chicagobutton').click(function() {
-        $('.home_text').animate({'color':'#FFF'},500).delay(800).animate({'color':'#222'});
+        $('.home_text').animate({'color':'#FFF'},500).delay(800).animate({'color':'#777'});
         $('#home_chicagobackground').fadeIn(500).delay(800).fadeOut(500);
     });
 
