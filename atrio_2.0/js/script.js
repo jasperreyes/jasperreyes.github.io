f114d7aaa944cc42227482730088127c7ffa2806
jasperreyes <reyes.jasper@gmail.com>
2014-03-03 17:41:16 -0600

    shit

--- atrio_2.0/js/script.js
+++ atrio_2.0/js/script.js
@@ -9,4 +9,14 @@ $(document).ready(function(){
         $('#logo_white').show();
         $('#logo_blue').hide();
     });
+
+    $('#homepage_textbox p').hide();
+    $('#homepage_textbox img').hide();
+    $('#homepage_textbox img').delay(1000).fadeIn(300);
+    
+    $('#homepage_textbox img').click(function() {
+    	$('#homepage_textbox img').fadeOut(300);
+    	$('#homepage_textbox p').delay(300).fadeIn(300);
+    });
+
 });
