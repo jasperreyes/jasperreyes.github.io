e571ceb16aa0e0d173e976d250d2b496d41623c9
jasperreyes <reyes.jasper@gmail.com>
2014-03-04 14:32:08 -0600

    added bootstrap, testing navbar

--- atrio_2.0/js/script.js
+++ atrio_2.0/js/script.js
@@ -39,6 +39,18 @@ jQuery(document).ready(function(){
     };
 
 
+
+$('[data-spy="scroll"]').each(function () {
+  var $spy = $(this).scrollspy('refresh')
+});
+
+$('#myScrollspy').on('activate.bs.scrollspy', function () {
+  alert('itsactive!');
+});
+
+
+
+
     $('#landingPageTextBox').hide();
     $('#bgimage').hide();
 
