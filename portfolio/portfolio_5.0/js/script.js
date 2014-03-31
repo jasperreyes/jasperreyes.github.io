ec1dcf23679c2f988467c54528afef244a890a0b
jasperreyes <reyes.jasper@gmail.com>
2014-03-31 13:35:11 -0500

    added responsive features

--- portfolio/portfolio_5.0/js/script.js
+++ portfolio/portfolio_5.0/js/script.js
@@ -18,7 +18,7 @@ jQuery(document).ready(function(){
 	$('.tmb_ttl').hide();
 	$('.tmb_dsc').hide();
 	$('.tmb').mouseenter(function() {
-		$('.tmb_img',this).animate({'opacity':'0.8'},200);
+		$('.tmb_img',this).animate({'opacity':'0.3'},200);
 		$('.tmb_ttl',this).fadeIn(200);
 		$('.tmb_dsc',this).fadeIn(200);
 	});
@@ -28,4 +28,37 @@ jQuery(document).ready(function(){
 		$('.tmb_dsc',this).fadeOut(200);
 	});
 	// END THUMBNAIL INTERACTIONS
+
+    // DETECTS TOUCH SCREEN DEVICE TO DISPLAY THUMBNAIL TITLE AND DESCRIPTION -----------------------------------------------
+    function is_touch_device() {
+     return (('ontouchstart' in window)
+          || (navigator.MaxTouchPoints > 0)
+          || (navigator.msMaxTouchPoints > 0));
+    }
+     
+    if (!is_touch_device()) {
+
+        $('.tmb_ttl').hide();
+        $('.tmb_dsc').hide(); 
+
+        $('.tmb').mouseenter(function() {
+            $('.tmb_ttl',this).fadeIn(300);
+            $('.tmb_dsc',this).fadeIn(300);
+        });
+
+        $('.tmb').mouseleave(function() {
+            $('.tmb_ttl',this).fadeOut(300);
+            $('.tmb_dsc',this).fadeOut(300);
+        });
+    }
+
+    else
+    {
+        $('.tmb_ttl').show();
+        $('.tmb_ttl').show();
+    }
+
+
+
+
 });
