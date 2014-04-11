c49cce51e83a7a7a0e4e02844f6ff8300ee415c4
jasperreyes <reyes.jasper@gmail.com>
2014-04-11 15:18:50 -0500

    added imgur

---
+++ sites/imgur/js/script.js
@@ -0,0 +1,99 @@
+jQuery(document).ready(function(){
+
+	// INITIAL SETUP
+	// $('#landing_container').hide();
+	$('#ideas_container').hide();
+	$('#final_container').hide();
+	$('#thanks_container').hide();
+	// END INITIAL SETUP
+
+	// LANDING
+	$('#landingbutton').hide();
+	$('#landingbutton').delay(1000).fadeIn(400);
+	// END LANDING
+
+	// FINAL
+	$('#upvote').hide();
+	$('#downvote').hide();
+	$('#finalbutton').hide();
+
+		// ARROW BEHAVIORS 
+		$('#up_clicked').hide();
+		$('#up_hover').hide();
+
+		$('#down_clicked').hide();
+		$('#down_hover').hide();
+
+		$('#uparrow').mouseenter(function() {
+			$('#up').hide();
+			$('#up_hover').show();
+		});
+		$('#uparrow').mouseleave(function() {
+			$('#up').show();
+			$('#up_hover').hide();
+		});
+		$('#uparrow').click(function() {
+			$('#up').hide();
+			$('#up_hover').hide();
+			$('#up_clicked').show();
+			$('#down').show();
+			$('#down_hover').hide();
+			$('#down_clicked').hide();
+
+			$('#upvote').show();
+			$('#downvote').hide();
+			$('#finalnote').hide();
+
+			$('#finalbutton').delay(1000).fadeIn(400);
+		});
+		$('#downarrow').mouseenter(function() {
+			$('#down').hide();
+			$('#down_hover').show();
+		});
+		$('#downarrow').mouseleave(function() {
+			$('#down').show();
+			$('#down_hover').hide();
+		});
+
+		$('#downarrow').click(function() {
+			$('#down').hide();
+			$('#down_hover').hide();
+			$('#down_clicked').show();
+			$('#up').show();
+			$('#up_hover').hide();
+			$('#up_clicked').hide();
+
+			$('#downvote').show();
+			$('#upvote').hide();
+			$('#finalnote').hide();
+
+			$('#finalbutton').delay(1000).fadeIn(400);
+		});
+
+		$('#finalbutton').click(function() {
+			$('#final_container').fadeOut(400);
+			$('#thanks_container').delay(400).fadeIn(400);
+		});
+		// END ARROW BEHAVIORS
+
+
+
+	$('#navbar').show();
+	$('#landingbutton').hide();
+    $('#landingbutton').delay(1000).fadeIn(400);
+
+    $('#landingbutton').click(function() {
+     $('#landing_container').fadeOut(400);
+     $('#final_container').hide();
+     $('#ideas_container').delay(400).fadeIn(400);
+    });
+
+	$('#ideasbutton').click(function() {
+	  // $('#ideas_container').animate({'top':'-100%'},400);
+	  $('#ideas_container').fadeOut(400);
+	  $('#final_container').delay(400).fadeIn(400);
+	});
+
+
+
+});
