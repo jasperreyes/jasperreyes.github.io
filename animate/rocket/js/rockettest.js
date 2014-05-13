fe975733c263e660a49dc9e2ab22cd7edb13742f
jasperreyes <reyes.jasper@gmail.com>
2014-05-13 16:59:05 -0500

    relative rocket movement

--- animate/rocket/js/rockettest.js
+++ animate/rocket/js/rockettest.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-	$('#stage1_container').css('bottom','20px');
+	// $('#stage1_container').css('bottom','20px');
 	$('#stage1_container').delay(1000).animate({'bottom':'200px'},2000,'easeInOutQuad');
 	$('#stage1_container').delay(300).animate({'bottom':'-50px'},2000,'easeInOutQuad');
 	$('#stage1_container').delay(300).animate({'bottom':'100px'},2000,'easeInOutQuad');
@@ -13,11 +13,11 @@ $(document).ready(function() {
 	$('#stage1_container').delay(300).animate({'bottom':'-1000px'},2500,'easeInOutQuad');
 
 
-	$('#stage2_container').css('bottom','520px');
-	$('#stage2_container').delay(1000).animate({'bottom':'700px'},2000,'easeInOutQuad');
-	$('#stage2_container').delay(300).animate({'bottom':'450px'},2000,'easeInOutQuad');
-	$('#stage2_container').delay(300).animate({'bottom':'600px'},2000,'easeInOutQuad');
-	$('#stage2_container').delay(300).animate({'bottom':'500px'},2000,'easeInOutQuad');
+	$('#stage2_container').css('bottom','-100px');
+	$('#stage2_container').delay(1000).animate({'bottom':'100px'},2000,'easeInOutQuad');
+	$('#stage2_container').delay(300).animate({'bottom':'-150px'},2000,'easeInOutQuad');
+	$('#stage2_container').delay(300).animate({'bottom':'0px'},2000,'easeInOutQuad');
+	$('#stage2_container').delay(300).animate({'bottom':'-100px'},2000,'easeInOutQuad');
 	$('#stage2_container').delay(300).animate({'bottom':'1000px'},2000,'easeInQuart');
 
 });
