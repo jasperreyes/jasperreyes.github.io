6c99ec8d0283cc9054b4ccbf92e697c798b84915
jasperreyes <reyes.jasper@gmail.com>
2014-07-13 21:21:04 -0500

    added mars, mountains, spaceship two

--- sites/spacex2/js/script.js
+++ sites/spacex2/js/script.js
@@ -1,10 +1,10 @@
 jQuery(document).ready(function(){
 
-	// $('#mars').mouseenter(function() {
-	// 	$('#mars_message').css('opacity','1');
-	// });
-	// $('#mars').mouseout(function() {
-	// 	$('#mars_message').css('opacity','0');
-	// });
+	$('#mars').mouseenter(function() {
+		$('#mars_message').css('visibility','visible');
+	});
+	$('#mars').mouseout(function() {
+		$('#mars_message').css('visibility','hidden');
+	});
 
 });
