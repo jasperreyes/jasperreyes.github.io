f570a294de568b33a06f6281bc9fbcddd27f8ce6
jasperreyes <reyes.jasper@gmail.com>
2014-07-16 16:01:27 -0500

    updated dragon graphic

--- sites/spacex2/js/script.js
+++ sites/spacex2/js/script.js
@@ -1,10 +1,7 @@
 jQuery(document).ready(function(){
 
-	$('#mars').mouseenter(function() {
-		$('#mars_message').css('visibility','visible');
-	});
-	$('#mars').mouseout(function() {
-		$('#mars_message').css('visibility','hidden');
+	$('#mars').click(function() {
+		$(this).fadeOut();
 	});
 
 });
