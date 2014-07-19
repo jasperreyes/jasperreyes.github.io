38bf9a06528f0199080cd814f55441696df738ba
jasperreyes <reyes.jasper@gmail.com>
2014-07-19 13:57:41 -0500

    separated css elements

--- sites/spacex2/js/script.js
+++ sites/spacex2/js/script.js
@@ -1,7 +1,21 @@
-jQuery(document).ready(function(){
+$(document).ready(function()
+{
 
-	$('#mars').click(function() {
-		$(this).fadeOut();
-	});
+	$('#mars_container').hide();
+
+	setTimeout(function()
+	{
+		$('#mars').addClass('marsHuge');
+	}, 30000);
+
+	setTimeout(function()
+	{
+		$('#earth_container').delay(35000).fadeOut('fast');
+	},1000);
+
+	setTimeout(function()
+	{
+		$('#mars_container').delay(35000).fadeIn(100);
+	}, 1000);
 
 });
