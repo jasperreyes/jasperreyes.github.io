66d5ed16106568d43130cb11865c234319a7bff1
jasperreyes <reyes.jasper@gmail.com>
2014-07-25 14:51:51 -0500

    refining earth/mars transition

--- sites/spacex2/js/script.js
+++ sites/spacex2/js/script.js
@@ -1,21 +1,21 @@
 $(document).ready(function()
 {
 
-	// $('#mars_container').hide();
+	$('#mars_container').hide();
 
-	// setTimeout(function()
-	// {
-	// 	$('#mars').addClass('marsHuge');
-	// }, 30000);
+	setTimeout(function()
+	{
+		$('#mars').addClass('marsHuge');
+	}, 40000);
 
-	// setTimeout(function()
-	// {
-	// 	$('#earth_container').delay(35000).fadeOut('slow');
-	// },1000);
+	setTimeout(function()
+	{
+		$('#earth_container').delay(45000).fadeOut(1000);
+	},1000);
 
-	// setTimeout(function()
-	// {
-	// 	$('#mars_container').delay(35000).fadeIn(100);
-	// }, 1000);
+	setTimeout(function()
+	{
+		$('#mars_container').delay(45000).fadeIn(1000);
+	}, 1000);
 
 });
