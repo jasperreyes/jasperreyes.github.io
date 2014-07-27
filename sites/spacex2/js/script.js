aa2f7789eefc846711ded08d5b5c5ad0e25c34b6
jasperreyes <reyes.jasper@gmail.com>
2014-07-27 00:07:08 -0500

    building launch page

--- sites/spacex2/js/script.js
+++ sites/spacex2/js/script.js
@@ -1,36 +1,53 @@
 $(document).ready(function()
 {
 	// $('#site_container').hide();
-	// $('#earth_container').hide();
-	// $('#mars_container').hide();
+	$('#earth_container').hide();
+	$('#mars_container').hide();
+	$('#logo').hide();
+	$('#relaunchbutton').hide();
 	// $('#launch').hide();
-	
-	// setTimeout(function()
-	// {
-	// 	$('#mars').addClass('marsHuge');
-	// }, 40000, 'easeInOutQuad');
 
-	// setTimeout(function()
-	// {
-	// 	$('#earth_container').delay(48000).fadeOut(1000);
-	// },1000);
+	$('.button').mouseenter(function() {
+		$(this).css('transform','scale(1.1)');
+	});
 
-	// setTimeout(function()
-	// {
-	// 	$('#mars_container').delay(48000).fadeIn(1000);
-	// }, 1000);
+	$('.button').mouseleave(function() {
+		$(this).css('transform','scale(1)');
+	});
 
-	// setTimeout(function()
-	// {
-	// 	$('#message_container').delay(15000).fadeIn(1000);
-	// }, 1000);
+
+	$('#relaunchbutton').click(function() {
+		location.reload();
+	});
+
 
 	// $('#launchbutton').click(function() {
-	// 	$('#launch').animate({'top':'-100%'},1000);
-	// 	$('#earth_container').hide();
+	// 	// $('#launch').fadeOut(1000);
+	// 	$('#launch').animate({'left':'-100%'}, 500, 'easeInQuad').delay(500).hide(0);
+	// 	$('#earth_container').show();
+
+	// 	setTimeout(function()
+	// 	{
+	// 		$('#earth_container').delay(37000).fadeOut(1000);
+	// 	},1000);
+
+	// 	setTimeout(function()
+	// 	{
+	// 		$('#mars_container').delay(37000).fadeIn(1000);
+	// 	}, 1000);
+
+	// 	setTimeout(function()
+	// 	{
+	// 		$('#logo').delay(55000).fadeIn(3000);
+	// 	}, 1000);
+
+	// 	setTimeout(function()
+	// 	{
+	// 		$('#relaunchbutton').delay(60000).fadeIn(3000);
+	// 	}, 1000);
+
 
 	// });
-
 
 });
 
