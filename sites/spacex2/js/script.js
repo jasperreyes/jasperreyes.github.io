cc5cc2928d4fa72c0f698352dbc5ffa5a6e7cb74
jasperreyes <reyes.jasper@gmail.com>
2014-07-28 10:49:15 -0500

    removed clouds for smaller devices

--- sites/spacex2/js/script.js
+++ sites/spacex2/js/script.js
@@ -1,76 +1,32 @@
 $(document).ready(function() {
 
-	$('#launch_container').fadeIn(1000);
+	// $('#launch_container').fadeIn(1000);
+	$('#site_container').show();
+	$('#mars_container').show();
+	$('#finish_container').show();
+	$('#logo').delay(11000).fadeIn(3000);
+	$('#relaunchButton').delay(3000).fadeIn(1000);
+	$('#links').delay(14000).fadeIn(1000);
+	$('#disclaimer').delay(15000).fadeIn(1000);
 
-	$('#launchButton').click(function() {
-		$('#launch_container').delay(500).fadeOut(500);
-		$('#site_container').delay(500).fadeIn(500);
-		$('#earth_container').delay(500).fadeIn(500);
-		$('#earth_container').delay(35000).fadeOut(500);
-		$('#mars_container').delay(35000).fadeIn(500);
-		$('#finish_container').delay(50000).fadeIn(500);
-	});
-});
+	// $('.button').mouseenter(function() {
+	// 	// $(this).css('background-color','#CC3232';
+	// 	$(this).animate({ "background-color":"#CC3232"},10000);
+	// });
 
-
-
-// $(document).ready(function()
+// .button:hover
 // {
-// 	$('#earth_container').hide();
-// 	$('#mars_container').hide();
-// 	$('#launch_container').hide();
-// 	$('#logo').hide();
-// 	$('#relaunchButton').hide();
-// 	$('#links').hide();
-// 	$('#disclaimer').hide();
-
-// 	$('#launch_container').fadeIn(500);
-
-// 	$('#relaunchButton').click(function() {
-// 		$('#site_container').fadeOut(500);
-
-// 		setTimeout(function()
-// 		{
-// 			location.reload();
-// 		}, 2000);
-
-// 	});
+//   background-color: #CC3232;
+//   // transition: 500ms;
+//   transform: scale(1.1);
 
 
-// 	$('#launchButton').click(function() {
-// 		$('#launch_container').delay(500).fadeOut(500);
-// 		$('#earth_container').delay(500).show();
-
-// 		setTimeout(function()
-// 		{
-// 			$('#earth_container').delay(37000).fadeOut(500);
-// 		},1000);
-
-// 		setTimeout(function()
-// 		{
-// 			$('#mars_container').delay(35500).fadeIn(500);
-// 		}, 1000);
-
-// 		setTimeout(function()
-// 		{
-// 			$('#logo').delay(50000).fadeIn(1000);
-// 		}, 1000);
-
-// 		setTimeout(function()
-// 		{
-// 			$('#relaunchButton').delay(52000).fadeIn(1000);
-// 		}, 1000);
-
-// 		setTimeout(function()
-// 		{
-// 			$('#links').delay(53000).fadeIn(1000);
-// 		}, 1000);
-
-// 		setTimeout(function()
-// 		{
-// 			$('#disclaimer').delay(54000).fadeIn(1000);
-// 		}, 1000);
-// 	});
-
-// });
-
+	// $('#launchButton').click(function() {
+	// 	$('#launch_container').delay(500).fadeOut(1000);
+	// 	$('#site_container').delay(500).fadeIn(1000);
+	// 	$('#earth_container').delay(500).fadeIn(1000);
+	// 	$('#earth_container').delay(37000).fadeOut(1000);
+	// 	$('#mars_container').delay(36000).fadeIn(1000);
+	// 	// $('#finish_container').delay(50000).fadeIn(1000);
+	// });
+});
