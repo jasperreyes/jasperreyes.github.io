3acbbbe9c120d332d17b9ecef86b0a6fa2c2d438
jasperreyes <reyes.jasper@gmail.com>
2014-07-28 11:17:47 -0500

    fixed js timing

--- sites/spacex2/js/script.js
+++ sites/spacex2/js/script.js
@@ -1,32 +1,37 @@
 $(document).ready(function() {
 
-	// $('#launch_container').fadeIn(1000);
-	$('#site_container').show();
-	$('#mars_container').show();
-	$('#finish_container').show();
-	$('#logo').delay(11000).fadeIn(3000);
-	$('#relaunchButton').delay(3000).fadeIn(1000);
-	$('#links').delay(14000).fadeIn(1000);
-	$('#disclaimer').delay(15000).fadeIn(1000);
+	$('.button').mouseenter(function() {
+		$(this).animate({'background-color':'#CC3232'},250);
+		$(this).css('transform','scale(1.1)');
+	});
 
-	// $('.button').mouseenter(function() {
-	// 	// $(this).css('background-color','#CC3232';
-	// 	$(this).animate({ "background-color":"#CC3232"},10000);
-	// });
+	$('.button').mouseleave(function() {
+		$(this).animate({'background-color':'#104E8B'},250);
+		$(this).css('transform','scale(1)');
+	});
 
-// .button:hover
-// {
-//   background-color: #CC3232;
-//   // transition: 500ms;
-//   transform: scale(1.1);
+	$('#launch_container').fadeIn(1000);
 
+	$('#launchButton').click(function() {
+		$('#launch_container').fadeOut(1000);
+		$('#site_container').fadeIn(1000);
+		$('#earth_container').fadeIn(1000);
+		$('#earth_container').delay(37000).fadeOut(1000);
+		$('#mars_container').delay(36000).fadeIn(1000);
+		$('#finish_container').delay(50000).fadeIn(1000);
+		$('#logo').delay(51000).fadeIn(3000);
+		$('#relaunchButton').delay(53000).fadeIn(1000);
+		$('#links').delay(54000).fadeIn(1000);
+		$('#disclaimer').delay(55000).fadeIn(1000);
+	});
 
-	// $('#launchButton').click(function() {
-	// 	$('#launch_container').delay(500).fadeOut(1000);
-	// 	$('#site_container').delay(500).fadeIn(1000);
-	// 	$('#earth_container').delay(500).fadeIn(1000);
-	// 	$('#earth_container').delay(37000).fadeOut(1000);
-	// 	$('#mars_container').delay(36000).fadeIn(1000);
-	// 	// $('#finish_container').delay(50000).fadeIn(1000);
-	// });
+	$('#relaunchButton').click(function() {
+		$('#site_container').fadeOut(1000);
+		$('#launch_container').fadeOut(1000);
+
+		setTimeout(function()
+		{
+			location.reload();
+		}, 1000);
+	});
 });
