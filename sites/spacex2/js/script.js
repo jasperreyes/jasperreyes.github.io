5e3b7ae774f6e4dd88093774a98fcb86278a1953
jasperreyes <reyes.jasper@gmail.com>
2014-07-27 21:33:57 -0500

    adjusting launch page timing

--- sites/spacex2/js/script.js
+++ sites/spacex2/js/script.js
@@ -1,53 +1,60 @@
 $(document).ready(function()
 {
-	// $('#site_container').hide();
 	$('#earth_container').hide();
 	$('#mars_container').hide();
-	$('#logo').hide();
-	$('#relaunchbutton').hide();
-	// $('#launch').hide();
+	$('#launch_container').hide();
+	// $('#logo').hide();
+	// $('#relaunchButton').hide();
+	// $('#links').hide();
+	// $('#disclaimer').hide();
 
-	$('.button').mouseenter(function() {
-		$(this).css('transform','scale(1.1)');
+	// $('#launch_container').fadeIn(1000);
+
+	$('#relaunchButton').click(function() {
+		$('#site_container').fadeOut(1000);
+
+		setTimeout(function()
+		{
+			location.reload();
+		}, 2000);
+
 	});
 
-	$('.button').mouseleave(function() {
-		$(this).css('transform','scale(1)');
+
+	$('#launchButton').click(function() {
+		$('#launch_container').delay(250).fadeOut(250);
+		// $('#earth_container').delay(250).show();
+
+		// setTimeout(function()
+		// {
+		// 	$('#earth_container').delay(37000).fadeOut(1000);
+		// },1000);
+
+		setTimeout(function()
+		{
+			$('#mars_container').delay(500).fadeIn(500);
+		}, 1000);
+
+		setTimeout(function()
+		{
+			$('#logo').delay(10000).fadeIn(1000);
+		}, 1000);
+
+		setTimeout(function()
+		{
+			$('#relaunchButton').delay(12000).fadeIn(1000);
+		}, 1000);
+
+		setTimeout(function()
+		{
+			$('#links').delay(14000).fadeIn(1000);
+		}, 1000);
+
+		setTimeout(function()
+		{
+			$('#disclaimer').delay(16000).fadeIn(2000);
+		}, 1000);
 	});
-
-
-	$('#relaunchbutton').click(function() {
-		location.reload();
-	});
-
-
-	// $('#launchbutton').click(function() {
-	// 	// $('#launch').delay(1000).fadeOut(1000);
-	// 	$('#launch').delay(1000).animate({'left':'-100%'}, 500, 'easeInQuad').delay(500).hide(0);
-	// 	$('#earth_container').delay(1000).show();
-
-	// 	setTimeout(function()
-	// 	{
-	// 		$('#earth_container').delay(37000).fadeOut(1000);
-	// 	},1000);
-
-	// 	setTimeout(function()
-	// 	{
-	// 		$('#mars_container').delay(37000).fadeIn(1000);
-	// 	}, 1000);
-
-	// 	setTimeout(function()
-	// 	{
-	// 		$('#logo').delay(55000).fadeIn(3000);
-	// 	}, 1000);
-
-	// 	setTimeout(function()
-	// 	{
-	// 		$('#relaunchbutton').delay(60000).fadeIn(3000);
-	// 	}, 1000);
-
-
-	// });
 
 });
 
