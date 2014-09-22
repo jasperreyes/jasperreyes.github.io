0ac324fb55ac46ce9b237dfe28a97020d6f070e2
jasperreyes <reyes.jasper@gmail.com>
2014-09-21 20:33:41 -0500

    deleted old files

--- script.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    $('a').mouseover( function() { 
-          $(this).css("color", "white");
-    });
-    $('a').mouseout( function() { 
-          $(this).css("color", "#66cc33");
-    });
-});
