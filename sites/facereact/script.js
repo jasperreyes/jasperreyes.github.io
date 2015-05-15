04ba65c196902ea4c50c1023e8129834f265755f
jasperreyes <reyes.jasper@gmail.com>
2015-05-14 23:34:57 -0500

    updated portfolio site

---
+++ sites/facereact/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+	// $('#button1').mouseenter(function() {
+	// 	$('#image_container').animate({'background-color':'red'}, 1000);
+	// });
+	// $('#button1').mouseleave(function() {
+	// 	$('#image_container').animate({'background-color':'green'}, 1000);
+	// });
+
+	$('#button1').hover(function() {
+		$('#image_container').css('background-color','red');
+		$('#image_container').css('background-color','blue');
+	});
+
+
+
+
+
+});
