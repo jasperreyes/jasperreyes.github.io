26f6d30b4dd4ad2174eb483cdbae7bb6aa94f313
Jasper <Jasper@Jaspers-MBP.wowway.com>
2015-11-29 17:27:20 -0600

    added landing page project

---
+++ portfolio/landingPage/js/script.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+
+	$('#center').mouseenter(function() {
+		$(this).addClass('hover');
+		$('object').addClass('opacity1');
+	});
+	$('#whiteStripe2').mouseout(function() {
+		$(this).removeClass('hover');
+		$('object').removeClass('opacity1');
+	});
+
+	$('object').mouseenter(function() {
+		$('this').addClass('hover');
+	});
+	$('object').mouseout(function() {
+		$('this').removeClass('hover');
+	});
+
+
+
+	// $('#whiteStripe2').mouseenter(function() {
+	// 	$(this).addClass('hover');
+	// 	$('object').addClass('opacity1');
+	// });
+	// $('#whiteStripe2').mouseout(function() {
+	// 	$(this).removeClass('hover');
+	// 	$('object').removeClass('opacity1');
+	// });
+
+	// $('object').mouseenter(function() {
+	// 	$('this').addClass('hover');
+	// });
+	// $('object').mouseout(function() {
+	// 	$('this').removeClass('hover');
+	// });
+
+
+});
