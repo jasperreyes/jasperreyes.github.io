378f4410fc0ac4439d357218c5da1cba3d63ccbe
Jasper Reyes <reyes.jasper@gmail.com>
2016-07-21 07:49:52 -0500

    initial push
    homepage structure, project page structure, partial responsiveness

---
+++ portfolio/6.2/js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+
+
+	// PROJECT EXIT BUTTON
+	// $('.project_exitbutton').hide();
+	$('.projectExitButton').css('bottom','-40px').delay(1000).animate({'bottom':'0px'}, 300);
+
+
+});
