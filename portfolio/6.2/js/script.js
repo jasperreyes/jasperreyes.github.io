1df53f8c4b91948e77d579514029c3cac21d2bf0
Jasper Reyes <reyes.jasper@gmail.com>
2016-08-28 00:29:49 -0500

    Updates
    - added additional projects
    - refactoring css
    - shortened classes and id names
    - added responsiveness to project pages
    - fixed project exit button

--- portfolio/6.2/js/script.js
+++ portfolio/6.2/js/script.js
@@ -1,9 +1,7 @@
 $(document).ready(function() {
 
-
 	// PROJECT EXIT BUTTON
-	// $('.project_exitbutton').hide();
-	$('.projectExitButton').css('bottom','-40px').delay(1000).animate({'bottom':'0px'}, 300);
+	$('.prjExitBtn').css('bottom','-40px').delay(1000).animate({'bottom':'0px'}, 300);
 
 
 });
