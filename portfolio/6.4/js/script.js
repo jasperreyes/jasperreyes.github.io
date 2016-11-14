fd6b74cdfeb152f0507b3598ca3edf6caf09922b
Jasper Reyes <reyes.jasper@gmail.com>
2016-11-14 00:08:00 -0600

    updates
    finalized mobile/touch screen states

--- portfolio/6.4/js/script.js
+++ portfolio/6.4/js/script.js
@@ -16,15 +16,7 @@ $(document).ready(function() {
 		return (('ontouchstart' in window) || (navigator.MaxTouchPoints > 0) || (navigator.msMaxTouchPoints > 0));
     }
      
-    if (!is_touch_device()) {
-
-    	// alert('this is not a touch device.');
-    }
-
-    else {
-    	$('.btn:hover').addClass('disableHover');
-
-    	// alert('this is a touch device.');
-
+    if (is_touch_device()) {
+    	$('.btn').toggleClass('touchBtn');
     }
 });
