6670a78539d8a0d713225833d4f4cf6a26c1c801
Jasper Reyes <reyes.jasper@gmail.com>
2016-11-24 20:36:42 -0600

    update
    added project page, responsive

--- portfolio/6.5/js/script.js
+++ portfolio/6.5/js/script.js
@@ -1,10 +1,11 @@
 $(document).ready(function() {
 
 	//PROJECT EXIT BUTTON
-	// $('.prjExitBtn').css('bottom','-40px').delay(1000).animate({'bottom':'0px'}, 300);
-	// $('.seeLessWork').hide();
+	$('.prjExitBtn').css('bottom','-40px').delay(1000).animate({'bottom':'0px'}, 300);
 
 	// TOGGLE BUTTON LOGIC
+	$('.seeLessWork').hide();
+
 	$('#addPrjBtn').click(function() {
 		$('#addPrjBox').toggle();
 		$('.seeLessWork').toggle();
