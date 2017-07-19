64fb6670416d290f2d776c5df653240a9a5f0f57
Jasper Reyes <reyes.jasper@gmail.com>
2017-07-18 20:26:28 -0700

    portfolio 6.6
    - fixed broken img links

--- portfolio/6.6/js/script.js
+++ portfolio/6.6/js/script.js
@@ -1,41 +1,10 @@
 $(document).ready(function() {
 
-	// TOGGLE BUTTON LOGIC ----------------------------------
-	// $('#addPrjBox').hide();
-	$('.addPrjToggleBtn').hide();
-	var addPrjStatus = 'collapsed';
-	console.log(addPrjStatus);
-
-	$('.addPrjToggleBtn').click(function() {
-
-		if (addPrjStatus === 'collapsed') {
-			addPrjStatus = 'expanded';
-			$('#addPrjBox').show();
-			$('#addPrjExpandBtn').hide();
-		}
-
-		else if (addPrjStatus === 'expanded') {
-			addPrjStatus = 'collapsed';
-			$('#addPrjBox').hide();
-			$('#addPrjExpandBtn').show();
-		}
-
-		console.log(addPrjStatus);
-	});
-
 	// NAVBAR TEXT CONTROL ------------------------------------
 	var navTxt = '<em>User Experience &amp; Product Design</em>',
 	  div = document.getElementById('navDsc');
 	div.insertAdjacentHTML( 'beforeend', navTxt);
 
-	// TOGGLE SELF ILLUSTRATION HOVER
-	// $('#selfTxt').hover(function() {
-	// 	$('#selfIllustrationDefault').toggle();
-	// 	$('#selfIllustrationHover').toggle();
-	// });
-
-	//PROJECT EXIT BUTTON
-	$('.prjExitBtn').css('bottom','-40px').delay(1000).animate({'bottom':'0px'}, 300);
 
 	// SCROLL TO TOP
 	$('#scrollToTopBtn').click(function() {
@@ -43,4 +12,33 @@ $(document).ready(function() {
 	});
 
 
+	// TOGGLE SELF ILLUSTRATION HOVER
+	// $('#selfTxt').hover(function() {
+	// 	$('#selfIllustrationDefault').toggle();
+	// 	$('#selfIllustrationHover').toggle();
+	// });
+
+
+	// TOGGLE BUTTON LOGIC ----------------------------------
+	// $('#addPrjBox').hide();
+	// $('.addPrjToggleBtn').hide();
+	// var addPrjStatus = 'collapsed';
+	// console.log(addPrjStatus);
+
+	// $('.addPrjToggleBtn').click(function() {
+
+	// 	if (addPrjStatus === 'collapsed') {
+	// 		addPrjStatus = 'expanded';
+	// 		$('#addPrjBox').show();
+	// 		$('#addPrjExpandBtn').hide();
+	// 	}
+
+	// 	else if (addPrjStatus === 'expanded') {
+	// 		addPrjStatus = 'collapsed';
+	// 		$('#addPrjBox').hide();
+	// 		$('#addPrjExpandBtn').show();
+	// 	}
+
+	// 	console.log(addPrjStatus);
+	// });
 });
