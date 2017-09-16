9db81aa9c1dee78b6ddb671926dc4728265b25bb
Jasper Reyes <reyes.jasper@gmail.com>
2017-09-16 07:56:13 -0700

    zwerpy
    - removed commented out voice commands from html file
    - uncommented add.commands

--- exploration/Interaction/voiceCommand5/JS/script.js
+++ exploration/Interaction/voiceCommand5/JS/script.js
@@ -1766,10 +1766,7 @@ $(document).ready(function() {
      };
 
       // Add our commands to annyang
-      // annyang.addCommands(commands);
-
-      // Start listening. You can call this here, or attach this call to an event, button, etc.
-      
+      annyang.addCommands(commands);      
     }
 
 });
